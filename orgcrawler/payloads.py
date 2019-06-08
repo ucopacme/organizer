97e3f30d0b9aeb12aae2609472ad001ca39b233e
Ashley Gould <agould@ucop.edu>
2019-06-07 18:43:41 -0700

    some new untested payload functions

--- orgcrawler/payloads.py
+++ orgcrawler/payloads.py
@@ -149,3 +149,24 @@ def check_cloudtrail_status(region, account):   # pragma: no cover
                 status="enabled",
             ))
     return dict(TrailAccounts=trail_accounts)
+
+
+def list_ec2_instances(region, account):    # pragma: no cover
+    '''
+    orgcrawler -r OrganizationAccountAccessRole --regions us-west-2,us-east-1 orgcrawler.payloads.list_ec2_instances | jq -r '.[].Regions[].Output.Reservations[].Instances[]| select(.State.Name == "running") | .PrivateIpAddress'
+    '''
+    client = boto3.client('ec2', region_name=region, **account.credentials)
+    response = client.describe_instances()
+    response.pop('ResponseMetadata')
+    return response
+
+
+def list_vpn_gateways(region, account):    # pragma: no cover
+    '''
+    orgcrawler -r OrganizationAccountAccessRole --regions us-west-2,us-east-1 orgcrawler.payloads.list_vpn_gateways | jq -r '.[].Regions[].Output.VpnGateways[] | select(.State == "available") | .VpcAttachments[].VpcId'
+    '''
+    client = boto3.client('ec2', region_name=region, **account.credentials)
+    response = client.describe_vpn_gateways()
+    response.pop('ResponseMetadata')
+    return response
+
